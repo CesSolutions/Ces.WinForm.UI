d823886dcae5d0bb7d5fc240faf6c62726d27771
Hadi <hadi.mazareei@gmail.com>
2023-07-18 23:27:10 +0330

    add title inside CesTextbox

--- Ces.WinForm.UI/CesTextBox.Designer.cs
+++ Ces.WinForm.UI/CesTextBox.Designer.cs
@@ -29,26 +29,40 @@
         private void InitializeComponent()
         {
             this.txtTextBox = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtTextBox
             // 
             this.txtTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtTextBox.Location = new System.Drawing.Point(9, 12);
+            this.txtTextBox.Location = new System.Drawing.Point(7, 28);
             this.txtTextBox.Name = "txtTextBox";
-            this.txtTextBox.Size = new System.Drawing.Size(149, 16);
+            this.txtTextBox.Size = new System.Drawing.Size(174, 16);
             this.txtTextBox.TabIndex = 0;
             this.txtTextBox.Enter += new System.EventHandler(this.txtTextBox_Enter);
             this.txtTextBox.Leave += new System.EventHandler(this.txtTextBox_Leave);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.ForeColor = System.Drawing.Color.Gray;
+            this.lblTitle.Location = new System.Drawing.Point(7, 7);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(174, 16);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.Visible = false;
             // 
             // CesTextBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.txtTextBox);
             this.Name = "CesTextBox";
             this.Padding = new System.Windows.Forms.Padding(3);
-            this.Size = new System.Drawing.Size(168, 40);
+            this.Size = new System.Drawing.Size(188, 52);
+            this.Load += new System.EventHandler(this.CesTextBox_Load);
             this.PaddingChanged += new System.EventHandler(this.CesTextBox_PaddingChanged);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.CesTextBox_Paint);
             this.Resize += new System.EventHandler(this.CesTextBox_Resize);
@@ -60,5 +74,6 @@
         #endregion
 
         private TextBox txtTextBox;
+        private Label lblTitle;
     }
 }
--- Ces.WinForm.UI/CesTextBox.cs
+++ Ces.WinForm.UI/CesTextBox.cs
@@ -15,7 +15,7 @@ namespace Ces.WinForm.UI
         public CesTextBox()
         {
             InitializeComponent();
-            SetTexBoxLocation();
+            ArrangeControls();
             Redraw();
         }
 
@@ -51,7 +51,7 @@ namespace Ces.WinForm.UI
             set
             {
                 cesBorderThickness = value;
-                SetTexBoxLocation();
+                ArrangeControls();
                 Redraw();
             }
         }
@@ -81,18 +81,78 @@ namespace Ces.WinForm.UI
         }
 
 
+        private bool cesShowTitle { get; set; }
+        public bool CesShowTitle
+        {
+            get { return cesShowTitle; }
+            set
+            {
+                cesShowTitle = value;
+                lblTitle.Visible = value;
+                ArrangeControls();
+            }
+        }
+
+
+        private string cesTitleText { get; set; }
+        public string CesTitleText
+        {
+            get { return cesTitleText; }
+            set
+            {
+                cesTitleText = value;
+                lblTitle.Text = value;
+            }
+        }
+
+        private System.Drawing.ContentAlignment cesTitleAlignment { get; set; }
+        public System.Drawing.ContentAlignment CesTitleAlignment
+        {
+            get { return cesTitleAlignment; }
+            set
+            {
+                cesTitleAlignment = value;
+                lblTitle.TextAlign = value;
+            }
+        }
+
+        [System.ComponentModel.Browsable(true)]
+        [System.ComponentModel.EditorBrowsable]
+        public Label CesLabelControl { get { return lblTitle; } }
+
+
         [System.ComponentModel.Browsable(true)]
         [System.ComponentModel.EditorBrowsable]
         public TextBox CesTextBoxControl { get { return txtTextBox; } }
 
 
+        private CesTextBoxAlignmentEnum cesTextBoxAlignment { get; set; } =
+            CesTextBoxAlignmentEnum.EdgeToCenter;
+        public CesTextBoxAlignmentEnum CesTextBoxAlignment
+        {
+            get { return cesTextBoxAlignment; }
+            set
+            {
+                cesTextBoxAlignment = value;
+                ArrangeControls();
+            }
+        }
+
+        private bool cesAutoHeight { get; set; }
+        public bool CesAutoHeight
+        {
+            get { return cesAutoHeight; }
+            set
+            {
+                cesAutoHeight = value;
+                ArrangeControls();
+            }
+        }
+
         private void CesTextBox_Paint(object sender, PaintEventArgs e)
         {
             Redraw();
         }
-
-
-
 
 
         private void Redraw()
@@ -173,6 +233,8 @@ namespace Ces.WinForm.UI
                         using (var sb = new SolidBrush(this.txtTextBox.Focused ? cesFocusColor : cesBackColor))
                         {
                             this.txtTextBox.BackColor = this.txtTextBox.Focused ? cesFocusColor : cesBackColor;
+                            this.lblTitle.BackColor = this.txtTextBox.Focused ? cesFocusColor : cesBackColor;
+
                             gp.CloseFigure();
                             g.FillPath(sb, gp);
                         }
@@ -186,23 +248,64 @@ namespace Ces.WinForm.UI
 
         private void CesTextBox_Resize(object sender, EventArgs e)
         {
-            SetTexBoxLocation();
+            ArrangeControls();
         }
 
-        private void SetTexBoxLocation()
-        {
-            this.txtTextBox.Size = new Size(
-                this.Width - (cesBorderThickness * 2) - this.Padding.Left - this.Padding.Right,
-                this.Height - (cesBorderThickness * 2) - this.Padding.Top - this.Padding.Bottom);
 
-            this.txtTextBox.Location = new Point(
-                (this.Width / 2) - (this.txtTextBox.Width / 2),
-                (this.Height / 2) - (this.txtTextBox.Height / 2));
+        private void ArrangeControls()
+        {
+            if (cesAutoHeight)
+                this.Height = cesShowTitle ?
+                    lblTitle.Height + txtTextBox.Height + this.Margin.Top + this.Margin.Bottom + (cesBorderThickness * 4) :
+                    txtTextBox.Height + this.Margin.Top + this.Margin.Bottom + (cesBorderThickness * 4);
+
+            if (cesShowTitle)
+            {               
+                //this.lblTitle.Size = new Size(
+                //    this.Width - (cesBorderThickness * 2) - this.Padding.Left - this.Padding.Right,
+                //    this.Height - (cesBorderThickness * 2) - this.Padding.Top - this.Padding.Bottom);
+
+                this.lblTitle.Width = this.Width - (cesBorderThickness * 4) - this.Padding.Left - this.Padding.Right;
+
+                if (cesTextBoxAlignment == CesTextBoxAlignmentEnum.CenterToEdge)
+                {
+                    this.lblTitle.Location = new Point(
+                        (this.Width / 2) - (this.lblTitle.Width / 2),
+                        (this.Height / 2) - lblTitle.Height - cesBorderThickness);
+                }
+
+                if (cesTextBoxAlignment == CesTextBoxAlignmentEnum.EdgeToCenter)
+                {
+                    this.lblTitle.Location = new Point(
+                        (this.Width / 2) - (this.lblTitle.Width / 2),
+                        cesBorderThickness * 2);
+                }
+            }
+
+            //this.txtTextBox.Size = new Size(
+            //    this.Width - (cesBorderThickness * 2) - this.Padding.Left - this.Padding.Right,
+            //    this.Height - (cesBorderThickness * 2) - this.Padding.Top - this.Padding.Bottom);
+
+            this.txtTextBox.Width = this.Width - (cesBorderThickness * 4) - this.Padding.Left - this.Padding.Right;
+
+            if (cesTextBoxAlignment == CesTextBoxAlignmentEnum.CenterToEdge)
+            {
+                this.txtTextBox.Location = new Point(
+                    (this.Width / 2) - (this.txtTextBox.Width / 2),
+                    cesShowTitle ? (this.Height / 2) + cesBorderThickness : (this.Height / 2) - (this.txtTextBox.Height / 2));
+            }
+
+            if (cesTextBoxAlignment == CesTextBoxAlignmentEnum.EdgeToCenter)
+            {
+                this.txtTextBox.Location = new Point(
+                    (this.Width / 2) - (this.txtTextBox.Width / 2),
+                    cesShowTitle ? this.Height - txtTextBox.Height - (cesBorderThickness * 2) : (this.Height / 2) - (this.txtTextBox.Height / 2));
+            }
         }
 
         private void CesTextBox_PaddingChanged(object sender, EventArgs e)
         {
-            SetTexBoxLocation();
+            ArrangeControls();
         }
 
         private void txtTextBox_Enter(object sender, EventArgs e)
@@ -214,5 +317,16 @@ namespace Ces.WinForm.UI
         {
             Redraw();
         }
+
+        private void CesTextBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public enum CesTextBoxAlignmentEnum
+    {
+        CenterToEdge,
+        EdgeToCenter,
     }
 }
