fad7c2fba2bc06fb6bc1a972a180f24cfced0efa
Hadi <hadi.mazareei@gmail.com>
2023-07-21 19:20:49 +0330

    add CesNotification. but not completed

---
+++ Ces.WinForm.UI/CesNotification/CesNotificationOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ces.WinForm.UI.CesNotification
+{
+    public class CesNotificationOptions
+    {
+        public static void Show()
+        {
+            var frm = new CesNotification();
+            frm.Show();
+        }
+    }
+}
