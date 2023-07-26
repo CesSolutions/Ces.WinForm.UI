82d6225d183856b3c155f2427937da267ee0c289
Hadi <hadi.mazareei@gmail.com>
2023-07-27 00:20:46 +0330

    add CesLabel but not completed

---
+++ Ces.WinForm.UI/CesLabel.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace Ces.WinForm.UI
+{
+    partial class CesLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CesLabel
+            // 
+            this.Name = "CesLabel";
+            this.Size = new System.Drawing.Size(121, 30);
+            this.TextChanged += new EventHandler(this.CesLabel_TextChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ces.WinForm.UI/CesLabel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ces.WinForm.UI
+{
+    public partial class CesLabel : System.Windows.Forms.Label
+    {
+        public CesLabel()
+        {
+            InitializeComponent();
+        }
+
+
+        private bool cesVertical { get; set; }
+        [System.ComponentModel.Category("CesLabel")]
+        [System.ComponentModel.Description("CesLabel can show string value in vertical direction.")]
+        public bool CesVertical
+        {
+            get { return cesVertical; }
+            set
+            {
+                cesVertical = value;
+                Redraw();
+            }
+        }
+
+
+        private float cesDegree { get; set; } = 90;
+        [System.ComponentModel.Category("CesLabel")]
+        public float CesDegree
+        {
+            get { return cesDegree; }
+            set
+            {
+                cesDegree = value;
+                Redraw();
+            }
+        }
+
+        private int cesOffsetX { get; set; }
+        [System.ComponentModel.Category("CesLabel")]
+        public int CesOffsetX
+        {
+            get { return cesOffsetX; }
+            set
+            {
+                cesOffsetX = value;
+                Redraw();
+            }
+        }
+
+
+        private int cesOffsetY { get; set; }
+        [System.ComponentModel.Category("CesLabel")]
+        public int CesOffsetY
+        {
+            get { return cesOffsetY; }
+            set
+            {
+                cesOffsetY = value;
+                Redraw();
+            }
+        }
+
+
+        private void CesLabel_TextChanged(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            using System.Drawing.Graphics g = this.CreateGraphics();
+            g.Clear(this.BackColor);
+
+            using System.Drawing.SolidBrush brush = new SolidBrush(this.ForeColor);
+
+            var textSize = g.MeasureString(this.Text, this.Font);
+            var rectangle = new RectangleF(0, 0, textSize.Width, textSize.Height);
+            float offsetX = 0;
+            float offsetY = 0;
+
+            if (!cesVertical)
+            {
+                offsetX = 0;
+                offsetY = 0;
+            }
+
+            if (CesDegree == 90)
+            {
+                offsetX = 0;
+                offsetY = -textSize.Height;
+            }
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.RotateTransform(cesVertical ? cesDegree : 0);
+            g.TranslateTransform(cesOffsetX, cesOffsetY);
+            g.DrawString(this.Text, this.Font, brush, rectangle);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            Redraw();
+        }
+    }
+}
