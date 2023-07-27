c011e1ab823b24e13dc5b1a07450b125158ee210
Hadi <hadi.mazareei@gmail.com>
2023-07-27 14:45:55 +0330

    Complete CesLabel

--- Ces.WinForm.UI/CesLabel.cs
+++ Ces.WinForm.UI/CesLabel.cs
@@ -33,8 +33,22 @@ namespace Ces.WinForm.UI
         }
 
 
+        private string cesText { get; set; }
+        [System.ComponentModel.Category("CesLabel")]
+        [System.ComponentModel.Description("CesLabel can show string value in vertical direction.")]
+        public string CesText
+        {
+            get { return cesText; }
+            set
+            {
+                cesText = value;
+                Redraw();
+            }
+        }
+
+
         private System.Drawing.ContentAlignment cesTextAlignment { get; set; }
-            = ContentAlignment.TopLeft;
+            = ContentAlignment.MiddleCenter;
         [System.ComponentModel.Category("CesLabel")]
         public System.Drawing.ContentAlignment CesTextAlignment
         {
@@ -47,32 +61,20 @@ namespace Ces.WinForm.UI
         }
 
 
-        public bool cesAutoSize { get; set; }
-        [System.ComponentModel.Category("CesLabel")]
-        public bool CesAutoSize
-        {
-            get { return cesAutoSize; }
-            set
-            {
-                cesAutoSize = value;
 
-                if (value)
-                    this.AutoSize = false;
-
-                Redraw();
-            }
-        }
+        // Methods
 
 
         private void Redraw()
         {
             System.Drawing.Graphics g = this.CreateGraphics();
             using System.Drawing.SolidBrush brush = new SolidBrush(this.ForeColor);
-            var textSize = g.MeasureString(this.Text, this.Font);
+            var textSize = g.MeasureString(cesText, this.Font);
             var rectangle = new RectangleF(0, 0, textSize.Width, textSize.Height);
             float offsetX = 0;
             float offsetY = 0;
 
+            // Set X,Y Offset for TranslateTransform
 
             if (cesDegree == CesLabelRotationDegreeEnum.Rotate0)
             {
@@ -119,45 +121,6 @@ namespace Ces.WinForm.UI
 
             if (cesDegree == CesLabelRotationDegreeEnum.Rotate90)
             {
-                //switch (this.TextAlign)
-                //{
-                //    case ContentAlignment.TopLeft:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.TopCenter:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.TopRight:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleLeft:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleCenter:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleRight:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomLeft:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomCenter:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomRight:
-                //        offsetX = 0;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //}
                 switch (cesTextAlignment)
                 {
                     case ContentAlignment.TopLeft:
@@ -166,81 +129,41 @@ namespace Ces.WinForm.UI
                         break;
                     case ContentAlignment.TopCenter:
                         offsetX = 0;
-                        offsetY = -((this.Width / 2 - textSize.Width / 2) + textSize.Height);
+                        offsetY = -(this.Width / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.TopRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = -textSize.Height;
+                        offsetX = 0;
+                        offsetY = -this.Width;
                         break;
                     case ContentAlignment.MiddleLeft:
-                        offsetX = this.Height / 2 - textSize.Height / 2;
+                        offsetX = this.Height / 2 - textSize.Width / 2;
                         offsetY = -textSize.Height;
                         break;
                     case ContentAlignment.MiddleCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = (this.Height / 2 - textSize.Height / 2) - textSize.Height;
+                        offsetX = this.Height / 2 - textSize.Width / 2;
+                        offsetY = -(this.Width / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.MiddleRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = (this.Height / 2 - textSize.Height / 2) - textSize.Height;
+                        offsetX = this.Height / 2 - textSize.Width / 2;
+                        offsetY = -this.Width;
                         break;
                     case ContentAlignment.BottomLeft:
-                        offsetX = 0;
-                        offsetY = (this.Height - textSize.Height) - textSize.Height;
+                        offsetX = this.Height - textSize.Width;
+                        offsetY = -textSize.Height;
                         break;
                     case ContentAlignment.BottomCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = (this.Height - textSize.Height) - textSize.Height;
+                        offsetX = this.Height - textSize.Width;
+                        offsetY = -(this.Width / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.BottomRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = (this.Height - textSize.Height) - textSize.Height;
+                        offsetX = this.Height - textSize.Width;
+                        offsetY = -this.Width;
                         break;
                 }
             }
 
             if (cesDegree == CesLabelRotationDegreeEnum.Rotate180)
             {
-                //switch (this.TextAlign)
-                //{
-                //    case ContentAlignment.TopLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.TopCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.TopRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.MiddleRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //    case ContentAlignment.BottomRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = -textSize.Height;
-                //        break;
-                //}
-
                 switch (cesTextAlignment)
                 {
                     case ContentAlignment.TopLeft:
@@ -248,118 +171,79 @@ namespace Ces.WinForm.UI
                         offsetY = -textSize.Height;
                         break;
                     case ContentAlignment.TopCenter:
-                        offsetX = -(this.Width / 2 - textSize.Width / 2) - textSize.Width;
-                        offsetY = 0;
+                        offsetX = -(this.Width / 2 + textSize.Width / 2);
+                        offsetY = -textSize.Height;
                         break;
                     case ContentAlignment.TopRight:
-                        offsetX = this.Width - textSize.Width - textSize.Width;
+                        offsetX = -this.Width;
                         offsetY = -textSize.Height;
                         break;
                     case ContentAlignment.MiddleLeft:
-                        offsetX = -textSize.Height;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -textSize.Width;
+                        offsetY = -(this.Height / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.MiddleCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -(this.Width / 2 + textSize.Width / 2);
+                        offsetY = -(this.Height / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.MiddleRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -this.Width;
+                        offsetY = -(this.Height / 2 + textSize.Height / 2);
                         break;
                     case ContentAlignment.BottomLeft:
-                        offsetX = 0;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -textSize.Width;
+                        offsetY = -this.Height;
                         break;
                     case ContentAlignment.BottomCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -(this.Width / 2 + textSize.Width / 2);
+                        offsetY = -this.Height;
                         break;
                     case ContentAlignment.BottomRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -this.Width;
+                        offsetY = -this.Height;
                         break;
                 }
             }
 
             if (cesDegree == CesLabelRotationDegreeEnum.Rotate270)
             {
-                //switch (this.TextAlign)
-                //{
-                //    case ContentAlignment.TopLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.TopCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.TopRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.MiddleLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.MiddleCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.MiddleRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.BottomLeft:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.BottomCenter:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //    case ContentAlignment.BottomRight:
-                //        offsetX = -textSize.Width;
-                //        offsetY = 0;
-                //        break;
-                //}
                 switch (cesTextAlignment)
                 {
                     case ContentAlignment.TopLeft:
-                        offsetX = 0;
+                        offsetX = -textSize.Width;
                         offsetY = 0;
                         break;
                     case ContentAlignment.TopCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = 0;
+                        offsetX = -textSize.Width;
+                        offsetY = this.Width / 2 - textSize.Height / 2;
                         break;
                     case ContentAlignment.TopRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = 0;
+                        offsetX = -textSize.Width;
+                        offsetY = this.Width - textSize.Width;
                         break;
                     case ContentAlignment.MiddleLeft:
-                        offsetX = 0;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -(this.Height / 2 + textSize.Width / 2);
+                        offsetY = 0;
                         break;
                     case ContentAlignment.MiddleCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -(this.Height / 2 + textSize.Width / 2);
+                        offsetY = this.Width / 2 - textSize.Height / 2;
                         break;
                     case ContentAlignment.MiddleRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = this.Height / 2 - textSize.Height / 2;
+                        offsetX = -(this.Height / 2 + textSize.Width / 2);
+                        offsetY = this.Width - textSize.Width;
                         break;
                     case ContentAlignment.BottomLeft:
-                        offsetX = 0;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -this.Height;
+                        offsetY = 0;
                         break;
                     case ContentAlignment.BottomCenter:
-                        offsetX = this.Width / 2 - textSize.Width / 2;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -this.Height;
+                        offsetY = this.Width / 2 - textSize.Height / 2;
                         break;
                     case ContentAlignment.BottomRight:
-                        offsetX = this.Width - textSize.Width;
-                        offsetY = this.Height - textSize.Height;
+                        offsetX = -this.Height;
+                        offsetY = this.Width - textSize.Height;
                         break;
                 }
             }
@@ -368,7 +252,7 @@ namespace Ces.WinForm.UI
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             g.RotateTransform((float)cesDegree);
             g.TranslateTransform(offsetX, offsetY);
-            g.DrawString(this.Text, this.Font, brush, rectangle);
+            g.DrawString(cesText, this.Font, brush, rectangle);
         }
 
         protected override void OnPaint(PaintEventArgs e)
