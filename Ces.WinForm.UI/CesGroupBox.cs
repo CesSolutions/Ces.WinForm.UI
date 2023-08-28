039e3e2c1ad310cbe2505f0c22d6fe4b1a6c56b1
Hadi <hadi.mazareei@gmail.com>
2023-08-28 23:30:48 +0330

    complete CesGroupBox and remove temporary files

--- Ces.WinForm.UI/CesGroupBox.cs
+++ Ces.WinForm.UI/CesGroupBox.cs
@@ -15,84 +15,98 @@ using System.Windows.Forms;
 
 namespace Ces.WinForm.UI
 {
-    //[Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(System.ComponentModel.Design.IDesigner))]
-    [System.ComponentModel.Designer(typeof(CesGroupBoxDesigner))]
-    public partial class CesGroupBox : UserControl
+   // [System.ComponentModel.Designer(typeof(CesGroupBoxDesigner))]
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(System.ComponentModel.Design.IDesigner))]
+    public partial class CesGroupBox : Infrastructure.CesControlBase
     {
         public CesGroupBox()
         {
             InitializeComponent();
+            CesTitleText = "Group Name";
+            CesTitlePosition = Infrastructure.CesTitlePositionEnum.Top;
+            CesShowTitle = true;
 
-            System.ComponentModel.TypeDescriptor.AddAttributes(this.pnlContainer,
-                new System.ComponentModel.DesignerAttribute(typeof(ContainerPanelDesigner)));
+
+            //System.ComponentModel.TypeDescriptor.AddAttributes(this.pnlContainer,
+            //    new System.ComponentModel.DesignerAttribute(typeof(ContainerPanelDesigner)));
         }
 
-
-        [System.ComponentModel.DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
-        [System.ComponentModel.Category("Ces Container Panel")]
-        public Panel ContainerPanel
+        private void CesGroupBox_Paint(object sender, PaintEventArgs e)
         {
-            get { return pnlContainer; }
+            this.GenerateBorder(this);
         }
+
+
+        //[System.ComponentModel.DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        //[System.ComponentModel.Category("Ces Container Panel")]
+        //public Panel ContainerPanel
+        //{
+        //    get { return pnlContainer; }
+        //}
     }
 
-    public class CesGroupBoxDesigner : System.Windows.Forms.Design.ParentControlDesigner
-    {
-        public override void Initialize(IComponent component)
-        {
-            base.Initialize(component);
-            var contentsPanel = ((CesGroupBox)this.Control).ContainerPanel;
-            this.EnableDesignMode(contentsPanel, "Ces Container Panel");
-        }
-        public override bool CanParent(Control control)
-        {
-            return false;
-        }
-        protected override void OnDragOver(DragEventArgs de)
-        {
-            de.Effect = DragDropEffects.None;
-        }
-        protected override IComponent[] CreateToolCore(System.Drawing.Design.ToolboxItem tool,
-            int x, int y, int width, int height, bool hasLocation, bool hasSize)
-        {
-            return null;
-        }
-    }
+    //public class CesGroupBoxDesigner : System.Windows.Forms.Design.ParentControlDesigner
+    //{
+    //    public override void Initialize(IComponent component)
+    //    {
+    //        base.Initialize(component);
+    //        //var contentsPanel = ((CesGroupBox)this.Control).ContainerPanel;
+    //        //this.EnableDesignMode(contentsPanel, "Ces Container Panel");
 
-    public class ContainerPanelDesigner : System.Windows.Forms.Design.ParentControlDesigner
-    {
-        public override System.Windows.Forms.Design.SelectionRules SelectionRules
-        {
-            get
-            {
-                System.Windows.Forms.Design.SelectionRules selectionRules = base.SelectionRules;
-                selectionRules &= ~System.Windows.Forms.Design.SelectionRules.AllSizeable;
-                return selectionRules;
-            }
-        }
-        protected override void PostFilterAttributes(System.Collections.IDictionary attributes)
-        {
-            base.PostFilterAttributes(attributes);
-            attributes[typeof(DockingAttribute)] = new DockingAttribute(DockingBehavior.Never);
-        }
-        protected override void PostFilterProperties(System.Collections.IDictionary properties)
-        {
-            base.PostFilterProperties(properties);
-            var propertiesToRemove = new string[] {
-            "Dock", "Anchor",
-            "Size", "Location", "Width", "Height",
-            "MinimumSize", "MaximumSize",
-            "AutoSize", "AutoSizeMode",
-            "Visible", "Enabled",
-        };
-            foreach (var item in propertiesToRemove)
-            {
-                if (properties.Contains(item))
-                    properties[item] = TypeDescriptor.CreateProperty(
-                        this.Component.GetType(),
-                        (PropertyDescriptor)properties[item],
-                        new BrowsableAttribute(false));
-            }
-        }
-    }
+    //        //if (this.Control is CesGroupBox)
+    //        //    this.EnableDesignMode(
+    //        //        ((CesGroupBox)this.Control).ContainerPanel, 
+    //        //        "Ces Container Panel");
+    //    }
+    //    //public override bool CanParent(Control control)
+    //    //{
+    //    //    return false;
+    //    //}
+    //    //protected override void OnDragOver(DragEventArgs de)
+    //    //{
+    //    //    de.Effect = DragDropEffects.None;
+    //    //}
+    //    //protected override IComponent[] CreateToolCore(System.Drawing.Design.ToolboxItem tool,
+    //    //    int x, int y, int width, int height, bool hasLocation, bool hasSize)
+    //    //{
+    //    //    return null;
+    //    //}
+    //}
+
+    //public class ContainerPanelDesigner : System.Windows.Forms.Design.ParentControlDesigner
+    //{
+    //    public override System.Windows.Forms.Design.SelectionRules SelectionRules
+    //    {
+    //        get
+    //        {
+    //            System.Windows.Forms.Design.SelectionRules selectionRules = base.SelectionRules;
+    //            selectionRules &= ~System.Windows.Forms.Design.SelectionRules.AllSizeable;
+    //            return selectionRules;
+    //        }
+    //    }
+    //    protected override void PostFilterAttributes(System.Collections.IDictionary attributes)
+    //    {
+    //        base.PostFilterAttributes(attributes);
+    //        attributes[typeof(DockingAttribute)] = new DockingAttribute(DockingBehavior.Never);
+    //    }
+    //    protected override void PostFilterProperties(System.Collections.IDictionary properties)
+    //    {
+    //        base.PostFilterProperties(properties);
+    //        var propertiesToRemove = new string[] {
+    //        "Dock", "Anchor",
+    //        "Size", "Location", "Width", "Height",
+    //        "MinimumSize", "MaximumSize",
+    //        "AutoSize", "AutoSizeMode",
+    //        "Visible", "Enabled",
+    //    };
+    //        foreach (var item in propertiesToRemove)
+    //        {
+    //            if (properties.Contains(item))
+    //                properties[item] = TypeDescriptor.CreateProperty(
+    //                    this.Component.GetType(),
+    //                    (PropertyDescriptor)properties[item],
+    //                    new BrowsableAttribute(false));
+    //        }
+    //    }
+    //}
 }
