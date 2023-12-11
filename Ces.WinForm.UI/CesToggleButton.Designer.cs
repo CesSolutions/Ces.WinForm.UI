59fe821e0ab5a7d3bd0a09eed2178587d5a1688b
Hadi <hadi.mazareei@gmail.com>
2023-12-11 20:02:50 +0330

    add CesToggleButton

---
+++ Ces.WinForm.UI/CesToggleButton.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Ces.WinForm.UI
+{
+    partial class CesToggleButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CesToggleButton
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "CesToggleButton";
+            this.Size = new System.Drawing.Size(90, 35);
+            this.Click += new System.EventHandler(this.CesToggleButton_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CesToggleButton_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ces.WinForm.UI/CesToggleButton.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ces.WinForm.UI
+{
+    [DefaultEvent("Click")]
+    public partial class CesToggleButton : UserControl
+    {
+        public CesToggleButton()
+        {
+            InitializeComponent();
+        }
+
+        private Color cesActiveColor { get; set; } = Color.DodgerBlue;
+        [Category("Ces ToggleButton")]
+        public Color CesActiveColor
+        {
+            get { return cesActiveColor; }
+            set
+            {
+                cesActiveColor = value;
+                this.Invalidate();
+            }
+        }
+
+        private Color cesInactiveColor { get; set; } = Color.LightGray;
+        [Category("Ces ToggleButton")]
+        public Color CesInactiveColor
+        {
+            get { return cesInactiveColor; }
+            set
+            {
+                cesInactiveColor = value;
+                this.Invalidate();
+            }
+        }
+
+        private Color cesToggleActiveColor { get; set; } = Color.White;
+        [Category("Ces ToggleButton")]
+        public Color CesToggleActiveColor
+        {
+            get { return cesToggleActiveColor; }
+            set
+            {
+                cesToggleActiveColor = value;
+                this.Invalidate();
+            }
+        }
+
+        private Color cesToggleInactiveColor { get; set; } = Color.DarkGray;
+        [Category("Ces ToggleButton")]
+        public Color CesToggleInactiveColor
+        {
+            get { return cesToggleInactiveColor; }
+            set
+            {
+                cesToggleInactiveColor = value;
+                this.Invalidate();
+            }
+        }
+
+        private bool cesToggle { get; set; }
+        [Category("Ces ToggleButton")]
+        public bool CesToggle
+        {
+            get { return cesToggle; }
+            set
+            {
+                cesToggle = value;
+                this.Invalidate();
+            }
+        }
+
+        private string cesToggleActiveText { get; set; } = "ON";
+        [Category("Ces ToggleButton")]
+        public string CesToggleActiveText
+        {
+            get { return cesToggleActiveText; }
+            set
+            {
+                cesToggleActiveText = value;
+                this.Invalidate();
+            }
+        }
+
+        private string cesToggleInactiveText { get; set; } = "OFF";
+        [Category("Ces ToggleButton")]
+        public string CesToggleInactiveText
+        {
+            get { return cesToggleInactiveText; }
+            set
+            {
+                cesToggleInactiveText = value;
+                this.Invalidate();
+            }
+        }
+
+        private bool cesShowToggleText { get; set; } = true;
+        [Category("Ces ToggleButton")]
+        public bool CessShowToggleText
+        {
+            get { return cesShowToggleText; }
+            set
+            {
+                cesShowToggleText = value;
+                this.Invalidate();
+            }
+        }
+
+        private void CesToggleButton_Paint(object sender, PaintEventArgs e)
+        {
+            using Graphics g = this.CreateGraphics();
+            using SolidBrush backgroundBrush = new SolidBrush(CesToggle ? CesActiveColor : CesInactiveColor);
+            using SolidBrush toggleBrush = new SolidBrush(CesToggle ? CesToggleActiveColor : CesToggleInactiveColor);
+            float offset = 1f;
+
+            g.Clear(this.BackColor);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Draw background
+            g.FillEllipse(
+                backgroundBrush,
+                new RectangleF(
+                    offset,
+                    offset,
+                    this.Height - (2 * offset),
+                    this.Height - (2 * offset)));
+
+            g.FillEllipse(
+                backgroundBrush,
+                new RectangleF(
+                    this.Width - offset,
+                    offset,
+                    -(this.Height - (2 * offset)),
+                    this.Height - (2 * offset)));
+
+            g.FillRectangle(
+                backgroundBrush,
+                new RectangleF(
+                    (this.Height / 2) + offset,
+                    offset,
+                    this.Width - this.Height - (2 * offset),
+                    this.Height - (2 * offset)));
+
+            //Draw toggle circle
+            if (CesToggle)
+                g.FillEllipse(
+                    toggleBrush,
+                    new RectangleF(
+                        this.Width - this.Height + 4,
+                        4,
+                        this.Height - 8,
+                        this.Height - 8));
+            else
+                g.FillEllipse(
+                    toggleBrush,
+                    new RectangleF(
+                        4,
+                        4,
+                        this.Height - 8,
+                        this.Height - 8));
+
+            if (!CessShowToggleText)
+                return;
+
+            using SolidBrush textBrush = new SolidBrush(CesToggle ? this.ForeColor:Color.Gray);
+            var textSize = g.MeasureString(CesToggle ? CesToggleActiveText : CesToggleInactiveText, this.Font);
+
+
+            g.DrawString(
+                CesToggle ? CesToggleActiveText : CesToggleInactiveText,
+                this.Font,
+                textBrush,
+                new PointF(
+                    CesToggle ? (this.Height / 2) : (this.Width - textSize.Width - (this.Height / 2)),
+                    (this.Height / 2) - (textSize.Height / 2) + 1));
+        }
+
+        private void CesToggleButton_Click(object sender, EventArgs e)
+        {
+            CesToggle = !CesToggle;
+        }
+    }
+}
