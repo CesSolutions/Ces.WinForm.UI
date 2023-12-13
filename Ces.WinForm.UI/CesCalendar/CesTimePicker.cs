37e19e547b31f8a17f6887dbbfb8c5ed9ca5d051
Hadi <hadi.mazareei@gmail.com>
2023-12-13 11:21:17 +0330

    change deafult padding

--- Ces.WinForm.UI/CesCalendar/CesTimePicker.cs
+++ Ces.WinForm.UI/CesCalendar/CesTimePicker.cs
@@ -16,6 +16,7 @@ namespace Ces.WinForm.UI.CesCalendar
         public CesTimePicker()
         {
             InitializeComponent();
+            this.CesPadding = new Padding(all: 3);
             this.ChildContainer = this.pnlChildControl;
         }
 
