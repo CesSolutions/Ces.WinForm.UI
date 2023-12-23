c257ec1cc0718446b704ea8f565ca55eed5129ab
Hadi <hadi.mazareei@gmail.com>
2023-12-24 01:02:56 +0330

    restore previous code

--- Ces.WinForm.UI/CesComboBox/CesSimpleComboBox.cs
+++ Ces.WinForm.UI/CesComboBox/CesSimpleComboBox.cs
@@ -167,7 +167,7 @@ namespace Ces.WinForm.UI.CesComboBox
         }
 
 
-        public void CesDataSource<T>(IList<T> dataSource)
+        public void CesDataSource<T>(IList<T> dataSource) where T : class
         {
             frmPopup = new CesSimpleComboBoxPopup();
             frmPopup.Deactivate += new EventHandler(frmDeactivated);
