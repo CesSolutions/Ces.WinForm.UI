4c0669057f6ec736d26d393688461c5bd33804f0
hadi mazareei <hadi.mazareei@live.com>
2024-07-26 19:46:59 +0330

    add CesDatePicker2

---
+++ Ces.WinForm.UI/CesCalendar/CesDatePicker2.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Ces.WinForm.UI.CesCalendar
+{
+    partial class CesDatePicker2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSelectedDate = new System.Windows.Forms.Label();
+            this.pnlChildControl = new System.Windows.Forms.Panel();
+            this.pbOpenCalendar = new System.Windows.Forms.PictureBox();
+            this.pnlChildControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpenCalendar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSelectedDate
+            // 
+            this.lblSelectedDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSelectedDate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblSelectedDate.Location = new System.Drawing.Point(5, 4);
+            this.lblSelectedDate.Name = "lblSelectedDate";
+            this.lblSelectedDate.Size = new System.Drawing.Size(140, 16);
+            this.lblSelectedDate.TabIndex = 1;
+            this.lblSelectedDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlChildControl
+            // 
+            this.pnlChildControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlChildControl.Controls.Add(this.pbOpenCalendar);
+            this.pnlChildControl.Controls.Add(this.lblSelectedDate);
+            this.pnlChildControl.Location = new System.Drawing.Point(5, 5);
+            this.pnlChildControl.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlChildControl.Name = "pnlChildControl";
+            this.pnlChildControl.Size = new System.Drawing.Size(168, 25);
+            this.pnlChildControl.TabIndex = 2;
+            // 
+            // pbOpenCalendar
+            // 
+            this.pbOpenCalendar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pbOpenCalendar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbOpenCalendar.Image = global::Ces.WinForm.UI.Properties.Resources.CesDatePicker;
+            this.pbOpenCalendar.Location = new System.Drawing.Point(148, 4);
+            this.pbOpenCalendar.Name = "pbOpenCalendar";
+            this.pbOpenCalendar.Size = new System.Drawing.Size(18, 16);
+            this.pbOpenCalendar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbOpenCalendar.TabIndex = 2;
+            this.pbOpenCalendar.TabStop = false;
+            this.pbOpenCalendar.Click += new System.EventHandler(this.pbOpenCalendar_Click);
+            // 
+            // CesDatePicker2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.pnlChildControl);
+            this.Name = "CesDatePicker2";
+            this.Size = new System.Drawing.Size(178, 35);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CesDatePicker2_Paint);
+            this.pnlChildControl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpenCalendar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Label lblSelectedDate;
+        private Panel pnlChildControl;
+        private PictureBox pbOpenCalendar;
+    }
+}
---
+++ Ces.WinForm.UI/CesCalendar/CesDatePicker2.cs
@@ -0,0 +1,152 @@
+﻿using System.ComponentModel;
+
+namespace Ces.WinForm.UI.CesCalendar
+{
+    [ToolboxItem(true)]
+    public partial class CesDatePicker2 : Infrastructure.CesControlBase
+    {
+        public CesDatePicker2()
+        {
+            InitializeComponent();
+            ChildContainer = this.pnlChildControl;
+
+            cln = new CesCalendar2();
+            cln.MonthCalendar.TodayDate = DateTime.Today;
+            ShowSelectedDate(cln.MonthCalendar.SelectionStart);
+        }
+
+        public event EventHandler<Ces.WinForm.UI.CesCalendar.Events.CesSelectionEvent> CesSelectionChanged;
+
+        // This Class Property
+        private Ces.WinForm.UI.CesForm.CesForm frm;
+        private Ces.WinForm.UI.CesCalendar.CesCalendar2 cln;
+        private Color currentBorderColor;
+
+        // Properties
+        private bool cesAlignToRight = false;
+        [System.ComponentModel.Category("Ces Date Picker")]
+        public bool CesAlignToRight
+        {
+            get { return cesAlignToRight; }
+            set
+            {
+                cesAlignToRight = value;
+            }
+        }
+
+        private bool cesShowInLongFormat = false;
+        public bool CesShowInLongFormat
+        {
+            get { return cesShowInLongFormat; }
+            set 
+            { 
+                cesShowInLongFormat = value;
+
+                ShowSelectedDate(cln.MonthCalendar.SelectionStart);
+            }
+        }
+
+        private void CesDatePicker2_Paint(object sender, PaintEventArgs e)
+        {
+            this.lblSelectedDate.BackColor = CesBackColor;
+            this.GenerateBorder(this);
+        }
+
+        private void pbOpenCalendar_Click(object sender, EventArgs e)
+        {
+            cln = new CesCalendar2();
+            cln.CesSelectionChanged += this.OnClose;
+            cln.Dock = DockStyle.Fill;
+            cln.Location = new Point(0, 0);
+
+            frm = new Ces.WinForm.UI.CesForm.CesForm();
+            frm.StartPosition = FormStartPosition.Manual;
+            frm.CesFormType = CesForm.CesFormTypeEnum.None;
+            frm.CesBorderColor = this.CesBorderColor;
+            frm.CesShowResizeIcon = false;
+            frm.CesBorderThickness = 1;
+            frm.TopMost = true;
+            frm.Size = new Size(cln.Width, cln.Height);
+
+            // Check frm size to fit in location. if will be out ot screen,
+            // another location shall be select automatically
+
+            var controlLocation = this.PointToScreen(Point.Empty);
+            var screenSize = Screen.PrimaryScreen.WorkingArea;
+            var datePickerRightLocation = 0;
+            var datePickerLeftLocation = 0;
+            var datePickerBottomLocation = controlLocation.Y + this.Height + frm.Height;
+
+            // Top Location
+            if (datePickerBottomLocation > screenSize.Height)
+                frm.Top = controlLocation.Y - frm.Height;
+            else
+                frm.Top = controlLocation.Y + this.Height;
+
+            // Left Location
+            if (CesAlignToRight)
+                datePickerLeftLocation = controlLocation.X - (frm.Width - this.Width);
+            else
+                datePickerRightLocation = controlLocation.X + frm.Width;
+
+            if (CesAlignToRight)
+            {
+                if (datePickerLeftLocation < 0)
+                    frm.Left = 0;
+                else
+                    frm.Left = controlLocation.X - (frm.Width - this.Width);
+            }
+            else
+            {
+                if (datePickerRightLocation > screenSize.Width)
+                    frm.Left = screenSize.Width - frm.Width;
+                else
+                    frm.Left = controlLocation.X;
+            }
+
+
+            // Show
+            frm.Controls.Add(cln);
+            frm.ShowDialog();
+        }
+
+        private void OnClose(object sender, UI.CesCalendar.Events.CesSelectionEvent e)
+        {
+            frm.Close();
+
+            if (CesSelectionChanged is not null)
+                CesSelectionChanged.Invoke(this, new UI.CesCalendar.Events.CesSelectionEvent
+                {
+                    Start = e.Start,
+                    End = e.End
+                });
+
+            ShowSelectedDate(e.Start);
+        }
+
+        private void ShowSelectedDate(DateTime? value)
+        {
+            if (value == null)
+                this.lblSelectedDate.Text = string.Empty;
+
+
+            if (CesShowInLongFormat)
+                this.lblSelectedDate.Text = value.Value.ToLongDateString();
+            else
+                this.lblSelectedDate.Text = value.Value.ToShortDateString();
+        }
+
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+
+            if (this.Enabled)
+                CesBorderColor = currentBorderColor;
+            else
+            {
+                currentBorderColor = CesBorderColor;
+                CesBorderColor = Color.Silver;
+            }
+        }
+    }
+}
