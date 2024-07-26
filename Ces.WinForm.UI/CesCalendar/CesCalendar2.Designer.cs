953f78b136118933d1c9f57fc82d20b1403dd6f7
hadi mazareei <hadi.mazareei@live.com>
2024-07-26 18:50:38 +0330

    remove unncessaryr using and add Calendar2

---
+++ Ces.WinForm.UI/CesCalendar/CesCalendar2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ces.WinForm.UI.CesCalendar
+{
+    partial class CesCalendar2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlContainer = new Panel();
+            MonthCalendar = new MonthCalendar();
+            pnlContainer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlContainer
+            // 
+            pnlContainer.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlContainer.Controls.Add(MonthCalendar);
+            pnlContainer.Location = new Point(3, 3);
+            pnlContainer.Name = "pnlContainer";
+            pnlContainer.Padding = new Padding(3);
+            pnlContainer.Size = new Size(234, 168);
+            pnlContainer.TabIndex = 0;
+            // 
+            // MonthCalendar
+            // 
+            MonthCalendar.Dock = DockStyle.Fill;
+            MonthCalendar.Location = new Point(3, 3);
+            MonthCalendar.Margin = new Padding(0);
+            MonthCalendar.Name = "MonthCalendar";
+            MonthCalendar.ShowTodayCircle = false;
+            MonthCalendar.TabIndex = 0;
+            MonthCalendar.TrailingForeColor = Color.Gold;
+            // 
+            // CesCalendar2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pnlContainer);
+            Name = "CesCalendar2";
+            Size = new Size(240, 175);
+            Paint += CesCalendar2_Paint;
+            pnlContainer.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlContainer;
+        public MonthCalendar MonthCalendar;
+    }
+}
---
+++ Ces.WinForm.UI/CesCalendar/CesCalendar2.cs
@@ -0,0 +1,49 @@
+﻿
+using System.ComponentModel;
+
+namespace Ces.WinForm.UI.CesCalendar
+{
+    [ToolboxItem(true)]
+    public partial class CesCalendar2 : Infrastructure.CesControlBase
+    {
+        private Color currentBorderColor;
+
+        public CesCalendar2()
+        {
+            InitializeComponent();
+            ChildContainer = this.pnlContainer;
+            cesMonthCalendar = this.MonthCalendar;
+        }
+
+        private MonthCalendar cesMonthCalendar;
+        public MonthCalendar CesMonthCalendar
+        {
+            get { return cesMonthCalendar; }
+            set {  cesMonthCalendar = value; }
+        }
+
+        #region Override Methods
+
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+
+            if (this.Enabled)
+            {
+                CesBorderColor = currentBorderColor;
+            }
+            else
+            {
+                currentBorderColor = CesBorderColor;
+                CesBorderColor = Color.Silver;
+            }
+        }
+
+        #endregion Override Methods
+
+        private void CesCalendar2_Paint(object sender, PaintEventArgs e)
+        {
+            this.GenerateBorder(this);
+        }
+    }
+}
